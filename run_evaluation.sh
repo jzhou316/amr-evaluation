279e8ef5b0ee826bb31316981bb0190cd010ea56
jzhou316 <31744226+jzhou316@users.noreply.github.com>
2020-11-11 18:21:14 -0500

    bug fix

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,8 +4,7 @@ set -e
 set -o pipefail
 
 if [[ -z $1 ]]; then
-    parsed_data="../transition-amr-parser/EXP/exp_graphmp-swaparc-ptrlast_o5_roberta-large-top24_act-pos-grh_vmask1_shiftpos1_ptr-lay6-h1_grh-lay123-h2-allprev_1in1out_cam-layall-h2-abuf_tis-embtop-comadd-bp1/models_e
-p120_seed42/beam10/test_checkpoint_wiki-smatch_top5-avg.wiki.amr"
+    parsed_data="../transition-amr-parser/EXP/exp_graphmp-swaparc-ptrlast_o5_roberta-large-top24_act-pos-grh_vmask1_shiftpos1_ptr-lay6-h1_grh-lay123-h2-allprev_1in1out_cam-layall-h2-abuf_tis-embtop-comadd-bp1/models_ep120_seed42/beam10/test_checkpoint_wiki-smatch_top5-avg.wiki.amr"
 else
     parsed_data=$1
 fi
