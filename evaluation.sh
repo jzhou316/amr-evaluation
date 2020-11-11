f90e2d5b477960c03a5652c5df916a92c6accc11
jzhou316 <31744226+jzhou316@users.noreply.github.com>
2020-11-11 17:10:29 -0500

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -2,7 +2,10 @@
 
 # Evaluation script. Run as: ./evaluation.sh <parsed_data> <gold_data>
 
-out=`python smatch/smatch.py --pr -f "$1" "$2"`
+ndigits=3
+smatch_path="../transition-amr-parser/smatch/smatch.py"    # to control the version to be v1.0.4
+
+out=`python ${smatch_path} --pr --significant $ndigits -f "$1" "$2"`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
@@ -10,7 +13,7 @@ echo 'Smatch -> P: '$pr', R: '$rc', F: '$fs
 
 sed 's/:[a-zA-Z0-9-]*/:label/g' "$1" > 1.tmp
 sed 's/:[a-zA-Z0-9-]*/:label/g' "$2" > 2.tmp
-out=`python smatch/smatch.py --pr -f 1.tmp 2.tmp`
+out=`python ${smatch_path} --pr --significant $ndigits -f 1.tmp 2.tmp`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
@@ -18,7 +21,7 @@ echo 'Unlabeled -> P: '$pr', R: '$rc', F: '$fs
 
 cat "$1" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 1.tmp
 cat "$2" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 2.tmp
-out=`python smatch/smatch.py --pr -f 1.tmp 2.tmp`
+out=`python ${smatch_path} --pr --significant $ndigits -f 1.tmp 2.tmp`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
